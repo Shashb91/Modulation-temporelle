9121998074cd853d8f2f93a31894c82dbb6b3e0a
shash <shashankan.bala@icloud.com>
2026-06-04 13:55:22 +0200

    Corrections mineures

--- schemas_numeriques.py
+++ schemas_numeriques.py
@@ -11,11 +11,11 @@ def LaxWendroff1D(data):
     data.U = np.zeros((data.N, data.M, 2))
     A = np.array([[0, 1/data.rho], [data.rho * data.c**2, 0]])
 
-    for n in range(0, data.N -1):
+    for n in range(0, data.N - 1):
         for i in range(1, data.M - 1):
             a1 = (data.dt / (2 * data.dx)) * A @ (data.U[n, i+1, :] - data.U[n, i-1, :])
             a2 = (0.5 * (data.dt / data.dx)**2) * (A @ A) @ (data.U[n, i+1, :] + data.U[n, i-1, :] - 2 * data.U[n, i, :])
-            data.U[n+1, i, :] = data.U[n, i, :] - a1 + a2 + data.dt/data.dx * data.S(data.f,(n+1)*data.dt) * (i == data.xs) * np.array([data.opt, not data.opt]).transpose()
+            data.U[n+1, i, :] = data.U[n, i, :] - a1 + a2 + data.dt/data.dx * data.S(data.f,(n+1)*data.dt) * (i == data.xs) * np.array([1,0]).transpose()
     return data.U
 
 def ADER41D(data):
@@ -35,15 +35,15 @@ def ADER41D(data):
     C = np.zeros((5, 2, 2))
 
     for s in range(0, 5):
-        terme_somme = np.zeros((2, 2))
+        a = np.zeros((2, 2))
         for m in range(0, 4):
-            terme_somme += gamma[s, m] * (data.dt/data.dx)** (m + 1) * np.linalg.matrix_power(A, m + 1)
-        C[s, :, :] = terme_somme
+            a += gamma[s, m] * (data.dt/data.dx)** (m + 1) * np.linalg.matrix_power(A, m + 1)
+        C[s, :, :] = a
 
     for n in range(0, data.N - 1):
         for i in range(2, data.M - 2):
             a1 = sum([C[s] @ data.U[n, i + s - 2, :] for s in range(0, 5)])
-            a2 = (data.dt / data.dx) * data.S(data.f,(n+1)*data.dt) * (i == data.xs) * np.array([float(data.opt), float(not data.opt)])
+            a2 = (data.dt / data.dx) * data.S(data.f,(n+1)*data.dt) * (i == data.xs) * np.array([data.opt, not data.opt])
             data.U[n + 1, i, :] = data.U[n, i, :] - a1 + a2
 
     return data.U

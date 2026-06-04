2ae0503b7e281c998c0c44eed23a46fb203a9fbb
shash <shashankan.bala@icloud.com>
2026-06-04 13:54:27 +0200

    Corrections des paramètres d'erreur

--- main.py
+++ main.py
@@ -7,30 +7,29 @@ Implementation de Lax-Wendroff 1D/2D
 """
 from tracer import *
 from erreur import *
-from analytique import *
 
-data1 = Donnee1D(M = 250, label = "Lax Wendroff")                                #Lax Wendroff
-data2 = Donnee1D(M = 250, label = "Analytique")                                  #ADER4
+data1 = Donnee1D(M = 250,N = 2, label = "Lax Wendroff")                                #Lax Wendroff
+data2 = Donnee1D(M = 250,N = 2, label = "Analytique")                                  #ADER4
 data3 = Donnee1D(M = 250, label = "ADER4")                                       #Solution analytique
 
-U_LW = LaxWendroff1D(data1)
-anim1D(data1)
-
-u = analytique1D(data2)
-anim1D(data2)
-
-U_ADER4 = ADER41D(data3)
-anim1D(data3)
-
-anim1D_comparaison(data1, data2)                                                 #Comparaison LaxWendroff VS Analytique
-anim1D_comparaison(data3, data2)                                                 #Comparaison ADER4 VS Analytique
+#U_LW = LaxWendroff1D(data1)
+#anim1D(data1)
+#
+#u = analytique1D(data2)
+#anim1D(data2)
+#
+#U_ADER4 = ADER41D(data3)
+#anim1D(data3)
+#
+#anim1D_comparaison(data1, data2)                                                 #Comparaison LaxWendroff VS Analytique
+#anim1D_comparaison(data3, data2)                                                 #Comparaison ADER4 VS Analytique
 
 #=============================================================
-#Etude de l'erreur
+#Etude de l'erreur t = 0.05s !
 #=============================================================
 
-eps = erreur1D(0.1, f = "LW")
+eps = erreur1D(0.05, f = "LW")
 erreur1D_trace(eps, f = "LW")
 
-eps = erreur1D(0.1, f = "ADER4")
+eps = erreur1D(0.05, f = "ADER4")
 erreur1D_trace(eps, f = "ADER4")

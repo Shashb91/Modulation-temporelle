aff12113e8061e2d6d21f90cc171ec3e83e06d30
shash <shashankan.bala@icloud.com>
2026-06-11 11:08:35 +0200

    Création des fichiers pour la résolution temporelle

---
+++ schema_mt.py
@@ -0,0 +1,5 @@
+from donnee import *
+from numpy.linalg import matrix_power
+from tqdm import trange
+ncols = 125                                                                       #largeur de la barre de chargement
+
